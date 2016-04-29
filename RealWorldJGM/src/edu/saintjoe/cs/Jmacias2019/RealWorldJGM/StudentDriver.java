976e92496ceba5c098bdbeb52f29b409d9963364
jmacias <jmacias@localhost>
2016-04-28 20:32:13 -0400

    First commit of driver class

---
+++ RealWorldJGM/src/edu/saintjoe/cs/Jmacias2019/RealWorldJGM/StudentDriver.java
@@ -0,0 +1,25 @@
+package edu.saintjoe.cs.Jmacias2019.RealWorldJGM;
+
+public class StudentDriver {
+
+	
+// See what we can do about our class and students
+			
+			int StudentCount = 0;
+				
+				Course cmp112 = new Course("CMP112A", "B, Capouch", "Core 125");
+			
+			Student nextStudent = new Student("Jared Ast", "M", 1, 19);
+			cmp112.students[studentsCount++] = nextStudent;
+			nextStudent = new Student("Jude Brace", "M", 2, 20);
+			cmp112.Students[studentCount++] = nextStudent;s
+			nextStudent = new Student("Zach Bobos", "M", 1, 19);
+			cmp112.Students[studentsCount++] = nextStudent;
+			nextStudent = new Student("Jacqueline Macias", "F", 1, 19);
+			cmp112.Students[studentsCount++] = nextStudent; 
+
+		}
+
+	}
+
+}
