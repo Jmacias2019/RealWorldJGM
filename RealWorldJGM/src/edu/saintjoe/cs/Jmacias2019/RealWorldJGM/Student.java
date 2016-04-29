fb7af1e7faf4b1a27148c488555540199a5f1f12
jmacias <jmacias@localhost>
2016-04-28 20:37:43 -0400

    Updated student

--- RealWorldJGM/src/edu/saintjoe/cs/Jmacias2019/RealWorldJGM/Student.java
+++ RealWorldJGM/src/edu/saintjoe/cs/Jmacias2019/RealWorldJGM/Student.java
@@ -2,9 +2,54 @@ package edu.saintjoe.cs.Jmacias2019.RealWorldJGM;
 
 public class Student {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		private String name;
+		private String gender;
+		private int classYr;
+		private int age;
+		
+		
+		/* Data members above, methods below */
+		
+		// Right now a "full data" constructor only
+		public Student(String newName, String newGender, int newClassYr, int newAge) {
+			name = newName;
+			gender = newGender;
+			classYr = newClassYr;
+			age = newAge;
+		}
+		
+		// Accessors and Mutators for the data members
+		public void setName(String newName) {
+			name = newName;
+		}
+		public String getName() {
+			return name;
+		}
+		
+		public void setAge(int newAge) {
+			age = newAge;
+		}
+		public int getAge() {
+			return age;
+		}
+		
+		public void setGender(String newGender) {
+			gender = newGender;
+		}
+		public String getGender() {
+			return gender;
+		}
 
+		public void setClassYr(int year) {
+			classYr = year;
+		}
+		public int getClassYr() {
+			return classYr;
+		}
+		
+		public String toString() {
+			return ("Student: " + name + " Gender: " + gender + " Class Year: " + classYr + " Age: " + age);
+		}
+		
+		
 	}
-
-}
