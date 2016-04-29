d3054166f5c827d02cab58f9f8d4a6f8599cc5f4
jmacias <jmacias@localhost>
2016-04-28 20:35:11 -0400

    Added two more classes

---
+++ RealWorldJGM/src/edu/saintjoe/cs/Jmacias2019/RealWorldJGM/Course.java
@@ -0,0 +1,10 @@
+package edu.saintjoe.cs.Jmacias2019.RealWorldJGM;
+
+public class Course {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ RealWorldJGM/src/edu/saintjoe/cs/Jmacias2019/RealWorldJGM/Student.java
@@ -0,0 +1,10 @@
+package edu.saintjoe.cs.Jmacias2019.RealWorldJGM;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
